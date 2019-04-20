13acc0303e4c3ced384bf98f486f0eda0df26693
Andrew <andrewryanogle@gmail.com>
2019-04-20 11:55:15 -0700

    reached mvp on project

--- api/server.js
+++ api/server.js
@@ -1,19 +1,31 @@
-const express = require('express');
+const express = require("express");
 
-const games = require('../games/gamesModel.js');
+const games = require("../games/gamesModel.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-  res.status(200).json({ api: 'up' });
+server.get("/", async (req, res) => {
+  res.status(200).json({ api: "up" });
 });
 
-server.get('/games', async (req, res) => {
-  const rows = await games.getAll();
+server.get("/games", async (req, res) => {
+  try {
+    const videoGames = await games.getAll();
+    res.status(200).json(videoGames);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-  res.status(200).json(rows);
+server.post("/games", async (req, res) => {
+  try {
+    const game = await games.insert(req.body);
+    res.status(201).json(game);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
 module.exports = server;
